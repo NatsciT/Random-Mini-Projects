2b40fee660f98069f90c0b3b9f1c7bb22e6dbbd1
natscit <victory8500@naver.com>
2023-03-25 00:46:54 +0900

    .

--- 2/simplefunc.asm
+++ 2/simplefunc.asm
@@ -1,39 +1,53 @@
 
 	global sim_strlen
 	global sim_strcpy
-
+	global sim_strcat
 
 	section .text
 
 ; long sim_strlen(void* str);
+; str == rdi
 sim_strlen:
 	xor	rax, rax
-
 __sim_strlen_loop:
 	cmp	byte [rdi + rax], 0
 	jz	__sim_strlen_end
 	inc	rax
 	jmp	__sim_strlen_loop
-
 __sim_strlen_end:
 	ret
 
 	
 ; long sim_strcpy(void* src, void* des, size_t count);
+; src == rdi
+; des == rsi
+; count == rdx
 sim_strcpy:
-	push	rbx
 	xor	rax, rax
-
 __sim_strcpy_loop:
 	cmp	rax, rdx
 	je	__sim_strcpy_end
-	mov	bl, byte [rdi + rax]
-	mov	byte [rsi + rax], bl
+	mov	dl, byte [rdi + rax]
+	mov	byte [rsi + rax], dl
 	cmp	byte [rdi + rax], 0
 	jz	__sim_strcpy_end
 	inc	rax
 	jmp	__sim_strcpy_loop
-
 __sim_strcpy_end:
-	pop	rbx
 	ret
+
+
+; long sim_strcat(void* des, void* str, size_t des_size);
+; des == rdi
+; str == rsi
+; des_size == rdx
+sim_strcat:
+	mov	rax, rdi
+	xor	rcx, rcx
+__sim_strcat_loop1:
+	cmp	byte [rdi + rcx], 0
+	jz	__sim_strcat_loop2
+	inc	rcx
+	jmp	__sim_strcat_loop1
+__sim_strcat_loop2:
+	
