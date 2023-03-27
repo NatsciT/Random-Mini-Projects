5ec60f1a62556154aa116e78f7ff4552d43ea1d6
natscit <victory8500@naver.com>
2023-03-27 12:06:09 +0900

    .

--- 2/demo.c
+++ 2/demo.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-
+extern long sim_strlen(char* str);
 extern long sim_strcpy(char* src, char* des, long count);
+extern long sim_strcat(char* des, char* str, long count);
 
 int main(int argc, char** argv)
 {
-	char empty[50] = "";
+	char str1[16] = "test1";
+	char str2[16] = "";
 
 	if(argc != 2)
 		return -1;
 
-	printf("%ld\n%s\n", sim_strcpy(argv[1], empty, 5), empty);
-	
+	puts("---- strcat ----");
+	printf("str1: %lxd, ret: %lxd\nstring: %s\n\n",
+			(unsigned long)str1,
+			(unsigned long)sim_strcat(str1, argv[1], 16),
+			str1);
+
+	puts("---- strcpy ----");
+	printf("before: %s\ncount: %ld\nafter: %s\n\n",
+			str2,
+			sim_strcpy(argv[1], str2, 16),
+			str2);
+
+	puts("---- strlen ----");
+	printf("length: %ld\n\n", sim_strlen(argv[1]));
+
 	return 0;
 }
--- 2/simplefunc.asm
+++ 2/simplefunc.asm
@@ -24,16 +24,18 @@ __sim_strlen_end:
 ; count == rdx
 sim_strcpy:
 	xor	rax, rax
+	dec	rdx
 __sim_strcpy_loop:
 	cmp	rax, rdx
 	je	__sim_strcpy_end
 	mov	r8b, byte [rdi + rax]
 	mov	byte [rsi + rax], r8b
-	cmp	byte [rdi + rax], 0
+	cmp	r8b, 0
 	jz	__sim_strcpy_end
 	inc	rax
 	jmp	__sim_strcpy_loop
 __sim_strcpy_end:
+	mov	byte [rsi + rax], 0
 	ret
 
 
@@ -44,10 +46,23 @@ __sim_strcpy_end:
 sim_strcat:
 	mov	rax, rdi
 	xor	rcx, rcx
+	xor	r9, r9
+	dec	rdx
 __sim_strcat_loop1:
 	cmp	byte [rdi + rcx], 0
-	jz	__sim_strcat_loop2
+	je	__sim_strcat_loop2
 	inc	rcx
 	jmp	__sim_strcat_loop1
 __sim_strcat_loop2:
-	
+	cmp	rcx, rdx
+	je	__sim_strcat_end
+	mov	r8b, byte [rsi + r9]
+	mov	byte [rdi + rcx], r8b
+	cmp	byte [rsi + r9], 0
+	je	__sim_strcat_end
+	inc	rcx
+	inc	r9
+	jmp	__sim_strcat_loop2
+__sim_strcat_end:
+	mov	byte [rdi + rcx], 0
+	ret
