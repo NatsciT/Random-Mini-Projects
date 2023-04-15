51e4717339f58c7df7a5884fbc2efe5c808c4bae
natscit <victory8500@naver.com>
2023-04-15 16:18:04 +0900

    .

--- 4_Bootloader/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dd if=/dev/zero of=floppy.img bs=1024 count=1440
-dd if=ldr.bin of=floppy.img seek=0 count=1 conv=notrunc
-genisoimage -o btldr.iso -V 'BTLDR' -input-charset iso8859-1 -b floppy.img -hide * . 
--- 4_Bootloader/ldr.asm
+++ 4_Bootloader/ldr.asm
@@ -2,18 +2,26 @@ bits 16
 
 org 0x7c00
 
+call	clear_screen
+
 mov	si, msg
 call	print_str
 jmp	$
 
+clear_screen:
+	mov	ah, 0x00
+	mov	al, 0x03
+	int	0x10
+	ret
+
 print_str:
 	lodsb
 	or	al, al
-	jz	print_str.done
+	jz	__print_str_done
 	mov	ah, 0x0E
 	int	0x10
 	jmp	print_str
-print_str.done:
+__print_str_done:
 	ret
 
 msg	db	"Hello, world!", 0
