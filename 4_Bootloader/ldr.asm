2e1df9cf6b879b70bfa189b5a47556e0336aabb5
natscit <victory8500@naver.com>
2023-04-17 17:26:51 +0900

    .

--- 4_Bootloader/ldr.asm
+++ 4_Bootloader/ldr.asm
@@ -1,16 +1,30 @@
-bits 16
+bits	16
 
-org 0x7c00
+org	0x7c00
 
-call	clear_screen
+main:
+	call	clear_screen
 
-mov	si, msg
-call	print_str
-jmp	$
+	mov	si, msg
+	call	print_str
+
+main.L1:
+	call	read_char
+	test	al, al
+	jz	main.L1
+
+	mov	bh, 0x00
+	mov	cx, 0x0001
+	mov	bl, 0x0B
+	call	print_char
+	call	move_cursor
+
+	jmp	main.L1
+
+
 
 clear_screen:
-	mov	ah, 0x00
-	mov	al, 0x03
+	mov	ax, 0x0003	; 80x25 text mode, color
 	int	0x10
 	ret
 
@@ -24,7 +38,29 @@ print_str:
 __print_str_done:
 	ret
 
-msg	db	"Hello, world!", 0
+; BH = page number CX = number of times to repeat AL = ASCII code BL = attrib
+print_char:
+	mov	ah, 0x09
+	int	0x10
+	ret
+
+read_char:
+	mov	ah, 0x00
+	int	0x16
+	ret
+
+move_cursor:
+	mov	ah, 0x03
+	mov	bh, 0x00
+	int	0x10
+	add	dl, 0x01
+	mov	ah, 0x02
+	int	0x10
+	ret
+
+
+
+msg	db	"Hello, world!", 0xD, 0xA, 0
 
 times	510 - ($ - $$)	db 0
 dw	0xAA55
